a4831421e29ea0e2b5d542657cc3524b912604b6
anitta <anittanelson40@gmail.com>
2024-08-18 22:02:32 +0530

    final

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+function userdatas(){
     let xhr = new XMLHttpRequest();
     console.log("xhr : " , xhr);
     
@@ -8,141 +8,234 @@ window.onload = function() {
     
     
     
-    xhr.onreadystatechange = function (){
-        console.log("readystate : " , xhr.readyState);
-    
-        if (xhr.readyState === 4) {
-            console.log("status : " , xhr.status);
-             
-            if(xhr.status === 200) {
-    
-                let response = xhr.response;
-                console.log("response : " , response);
-                console.log("typeof(response) : ", typeof(response));
-    
-                let datas = JSON.parse(response);
-               console.log("datas : " , datas);
-    
-                let txt = document.getElementById('text');
-                console.log('text : ',txt);
-    
-                 let product = datas.find(item => item.id ===1);
-                 console.log('product',product);
-    
-                 if(product){
-                    txt.innerHTML=`
-                    <div>${product.title}</div>
-                    <div style="font-size: 20px; font-weight: 200;padding: 50px 0px 0px 0px">${product.description}</div>
-                    <div style="padding:40px 0px 0px 0px;">
-                    <button style="font-size:18px; font-weight:100; background-color: rgb(252,93,53); border:none; padding:10px; color:white;">Add To Cart</button>
-                    <button style="font-size:18px; font-weight:300; background-color:rgb(255,216,20); border:none; padding:10px;color:white;">Buy Now</button>
-                    </div>
-                    `;
-
-                    image.innerHTML=`
-                    <div><img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" style="width:500px; height:500px;" alt=""></div>
-                    `
-                  
-
-                 }
-                 else{
-    
-                    txt.innerHTML= `
-                    <p> not workingggg</p>
-                    `
-                 }
-                 let second = document.getElementById('text2');
-                console.log("text2 : " , second);
-
-                let product1 = datas.find(item => item.id ===2);
-                console.log('product1',product1);
-
-                if(product1){
-
-                    second.innerHTML=`
-                    <div>${product1.title}</div>
-                    <div style="font-size: 20px; font-weight: 200;padding: 50px 0px 0px 0px">${product1.description}</div>
-                    <div style="padding:40px 0px 0px 0px;">
-                    <button style="font-size:18px; font-weight:100; background-color: rgb(252,93,53); border:none; padding:10px;color:white; ">Add To Cart</button>
-                    <button style="font-size:18px; font-weight:300; background-color:rgb(255,216,20); border:none; padding:10px;color:white;">Buy Now</button>
-                    </div>
-                    
-                    `;
-                    image2.innerHTML=`
-                    <div><img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" style="width:500px; height:500px;" alt=""></div>`
-                }
-                else{
-                    second.innerHTML=`
-                    <p>not wrking</p>
-                    `;
-                }
-
-                let third = document.getElementById('text3');
-                console.log("text3 : " , third);
-
-                let product2 = datas.find(item => item.id ===3);
-                console.log('product2',product2);
-
-                if(product2){
-
-                    third.innerHTML=`
-                    <div>${product2.title}</div>
-                    <div style="font-size: 20px; font-weight: 200;padding: 50px 0px 0px 0px">${product2.description}</div>
-                     <div style="padding:40px 0px 0px 0px;">
-                     <button style="font-size:18px; font-weight:100; background-color: rgb(252,93,53); border:none; padding:10px;color:white; ">Add To Cart</button>
-                    <button style="font-size:18px; font-weight:300; background-color:rgb(255,216,20); border:none; padding:10px;color:white;">Buy Now</button>
-                    </div>
-                    `;
-                    image3.innerHTML=`
-                    <div><img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" style="width:500px; height:500px;" alt=""></div>`
-                }
-                else{
-                    third.innerHTML=`
-                    <p>not wrking</p>
-                    `;
-                }
-
-               
-               
-
-                let section1 = [15, 16, 17,6,7,8];
-
-                // let sub = document.getElementsByClassName('img');
-                let sub1 = document.getElementsByClassName('title');
-                let sub2 = document.getElementsByClassName('description');
-                let sub3 = document.getElementsByClassName('price');
-                document.getElementsByClassName('img')[0].src = 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg';
-                document.getElementsByClassName('img1')[0].src = 'https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg';
-                document.getElementsByClassName('img2')[0].src = 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg';
-
-
-                document.getElementsByClassName('img3')[0].src = 'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg';
-                document.getElementsByClassName('img4')[0].src = 'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg';
-                document.getElementsByClassName('img5')[0].src = 'https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg';
-
-                section1.forEach((id, index) => {
-                    let product = datas.find(item => item.id === id);
-                    if (product) {
-                        if (index < sub1.length) {
-                            console.log(`Setting image for index ${index}: ${product.img}`);
-                            // sub[index].src = product.img;
-                            sub1[index].innerHTML = product.title;
-                            sub2[index].innerHTML = product.description.slice(0,100)+"........";
-                            sub3[index].innerHTML = product.price;
-                        } else {
-                            console.log('Index out of bounds for sub arrays');
-                        }
-                    } else {
-                        console.log(`Product with id ${id} not found`);
-                    }
-                });
-               
-
-    
-
+        xhr.onreadystatechange = function (){
+            console.log("readystate : " , xhr.readyState);
+        
+            if (xhr.readyState === 4) {
+                console.log("status : " , xhr.status);
                 
+                if(xhr.status === 200) {
+        
+                    let response = xhr.response;
+                    console.log("response : " , response);
+                    console.log("typeof(response) : ", typeof(response));
+        
+                    let datas = JSON.parse(response);
+                    console.log("datas : " , datas);
+            
+                    // let hi = document.querySelector('.carousel-inner');
+                    // console.log("carousel-inner :", hi);
+                    
+                    // let box = '';
+                    // for (let i = 0; i < 3; i++) {
+                    //     box += `
+                        
+                    //          <div class="carousel-item  active">
+                    //             <div><img src="${datas[0].image}" style="width:500px; height:500px;"></div> 
+                    //             <!--<div><h2>${datas[i].title}</h2></div>--!>
+                    //         </div>
+                    //         <div class="carousel-item ">
+                    //            <div><img src="${datas[i+1].image}" style="width:500px; height:500px;"></div> 
+                    //         </div>
+                    //         <div class="carousel-item ">
+                    //              <div><img src="${datas[i+2].image}" style="width:500px; height:500px;"></div> 
+                    //         </div>
+                    
+                    //     `;
+                    let arr =["men's clothing","jewelery","women's clothing","electronics"];
+                    console.log(arr)
+                    let hi = document.getElementById('carousel-inner1')
+                    let box = '';
+                    let container = document.getElementById('jewelery');
+                    let box2 ='';
+                    let section = document.getElementById('womens')
+                    let box3 = '';
+                    let hello = document.getElementById('electronics')
+                    let box4 = '';
+                    
+                    for (let i = 0; i < datas.length; i++) {
 
+                        for(j = 0 ;j<arr.length-3;j++){
+                            if(datas[i].category === arr[0]){
+                                box = box+ `
+                                <div class="carousel-item ${i === 0 ? 'active' : ''} ">
+                                    <div class = "d-flex justify-content-center align-items-center">
+                                        
+                                            <div>
+                                                <div class = " fs-2 fw-bold">${datas[i].title}</div>
+                                                <div style="width:700px; " class="pt-4 fs-5"><p>${datas[i].description}</p></div>
+                                                
+                                                <button  onClick ="handleClick(${datas[i].id})" class="p-2 px-4 bg-warning border-0">Add to cart</button>
+                                            </div>
+                                            
+                                            <div>
+                                                <img src="${datas[i].image}" alt="Product Image" class = "image1" style="width:500px; height:500px; padding:20px;">
+                                            </div>
+                                       
+                                    </div>
+                                </div>
+                                `;
+                            }
+                            
+                            if(datas[i].category === arr[1]){
+                                box2 = box2+ `
+                                    <div class="shadow p-3 mb-5 bg-body rounded text-center keychain ">
+                                        <div>
+                                            <div class = "hardy fw-bold ">${datas[i].title.slice(0,20)+"..."}</div>
+                                            <div style=" " class="pt-4  dragon"><p>${datas[i].description.slice(0,30)+"..."}</p></div>
+                                            
+                           
+                                        </div>
+                                        
+                                        <div class="text-center">
+                                            <img src="${datas[i].image}" alt="Product Image" class = "image1" style="width:200px; height:200px; padding:20px;">
+                                        </div>
+                                         <div class="pt-4 text-center"><button  onClick ="handleClick(${datas[i].id})"  class="p-2 px-4 bg-warning border-0">Add to cart</button></div>
+
+                                    </div>
+                                    
+                                   
+                                
+                                `;
+                            }
+                            if(datas[i].category === arr[2]){
+                                box3 = box3+ `
+                                    <div class="shadow p-3 mb-5 bg-body rounded text-center key">
+                                        <div>
+                                            <div class = "hardy fw-bold ">${datas[i].title.slice(0,15)+"..."}</div>
+                                            <div style=" " class="pt-4  dragon"><p>${datas[i].description.slice(0,15)+"..."}</p></div>
+                                            
+                           
+                                        </div>
+                                        
+                                        <div class="text-center">
+                                            <img src="${datas[i].image}" alt="Product Image" class = "image1" style="width:185px; height:185px; padding:20px;">
+                                        </div>
+                                         <div class="pt-4 text-center"><button onClick ="handleClick(${datas[i].id})"  class="p-2 px-4 bg-warning border-0">Add to cart</button></div>
+
+                                    </div>
+                                    
+                                   
+                                
+                                `;
+                            }
+
+                            if(datas[i].category === arr[3]){
+                                box4 = box4+ `
+                                    <div class="shadow p-3 mb-5 bg-body rounded text-center key">
+                                        <div>
+                                            <div class = "hardy fw-bold ">${datas[i].title.slice(0,20)+"..."}</div>
+                                            <div style=" " class="pt-4  dragon"><p>${datas[i].description.slice(0,15)+"..."}</p></div>
+                                            
+                           
+                                        </div>
+                                        
+                                        <div class="text-center">
+                                            <img src="${datas[i].image}" alt="Product Image" class = "image1" style="width:185px; height:185px; padding:20px;">
+                                        </div>
+                                         <div class="pt-4 text-center"><button onClick ="handleClick(${datas[i].id})" class="p-2 px-4 bg-warning border-0">Add to cart</button></div>
+
+                                    </div>
+                                    
+                                   
+                                
+                                `;
+                            }
+                        // box += `
+                       
+                        //     <div class="carousel-item ${i === 0 ? 'active' : ''} ">
+                        //         <div class="hi">
+                        //             <div style="padding-left:40px; text-align:left;">
+                        //                 <div ><h2 class="fs-2 fw-bold">${datas[i].title}</h2></div>
+                        //                 <div style="width:700px; " class="pt-4 fs-5"><p>${datas[i].description}</p></div>
+                        //                 <div class="pt-4"><button class="p-2 px-4 bg-warning border-0">Add To Cart</button></div>
+                        //             </div>
+                        //             <div><img src="${datas[i].image}" style="width:500px; height:500px; padding:20px;"></div>
+
+                        //         </div>
+                        //     </div>
+                        
+                        // `;
+                    }
+                    
+                    hi.innerHTML = box;
+                    
+                    container.innerHTML=box2;
+
+                    section.innerHTML=box3;
+
+                    hello.innerHTML=box4
+                    
+                    
+                      
+                }
+                  
+                
             }
+            
         }
     }
-}    
+}
+function handleClick(id){
+    console.log("button clicked");
+    console.log("button clicked id: ", id);
+    window.location.href = `view.html?id=${id}`
+    return;
+}
+ function product(){
+    console.log("loading....");
+
+    let location = window.location;
+    console.log("location : ",location);
+
+    let querystring = location.search;
+    console.log("querystring : ",querystring);
+
+    let urlparams = new URLSearchParams(querystring);
+    console.log("url" , urlparams);
+
+    let id = urlparams.get("id");
+    console.log("id : ",id);
+
+
+    let xhr = new XMLHttpRequest();
+    console.log("xhr:",xhr);
+    xhr.open("get", `https://fakestoreapi.com/products/${id} `)
+
+    xhr.send();
+
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4){
+            console.log("staus : ",xhr.status);
+        
+        if (xhr.status === 200){
+            console.log("success");
+
+        let userData = xhr.response;
+        console.log("userData: ",userData);
+
+        let parsed_userData = JSON.parse (userData);
+        console.log("parsed_userData ",parsed_userData);
+
+
+        let section2 = document.getElementById('first');
+
+        section2.innerHTML=parsed_userData.title
+
+        let section3 = document.getElementById('second');
+        section3.innerHTML=parsed_userData.description
+
+        let section4 = document.getElementById('third');
+        section4.innerHTML=parsed_userData.category
+
+        let section5 =document.getElementById('img4');
+        section5.innerHTML=`<img src="${parsed_userData.image}" style="width:300px; height:300px;">`
+
+
+        }
+        }
+
+    }
+
+    
+}
